8cba5d4342f21ce02479b272df456de09b44caa2
Juan Carlos Torterolo Pedroza <juan.torterolo@gmail.com>
2022-07-19 23:47:08 -0300

    adicionando site

---
+++ src/Components/Card/CardStyled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const CardBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #6AA;
+    width: 15em;
+    height: 20em;
+    align-items: center;
+    margin: 2em 2em;
+`
