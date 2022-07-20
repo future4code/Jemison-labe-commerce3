a3be7f3885572a295dabeda589a6fb283c4d0256
Cristiane de Lima Borges <69880452+crixlima@users.noreply.github.com>
2022-07-19 22:45:31 -0300

    codigo-incial

---
+++ src/mockapDados.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const HomeList=[
+    { imagem: "",
+    nome:"Nave",
+    preco: 100.00,      
+
+    },
+
+    { imagem: "",
+    nome:"Onibus Espacial",
+    preco: 200.00,  
+
+    },
+
+    { imagem: "",
+    nome:"Onibus Espacial",
+    preco: 200.00,  
+    },
+
+    { imagem: "",
+    nome:"Foguete Luz",
+    preco: 200.00,  
+    },
+
+    { imagem: "",
+    nome:"Nave Estrelar",
+    preco: 200.00,  
+    },
+
+    { imagem: "",
+    nome:"Foguete Espacial",
+    preco: 200.00,  
+    },
+
+]
